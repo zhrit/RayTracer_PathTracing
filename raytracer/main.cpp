3e114141ecfe58ded46848957c6e0cdc002ea70d
zhanghao11 <zhanghao11@corp.netease.com>
2021-06-11 18:22:25 +0800

    background

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+color ray_color(const ray& r)
+{
+	vec3 unit_direction = uint_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main()
 {
-	const int image_width = 256;
-	const int image_height = 256;
+	// Image
+	const double aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// Camera
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
+
+	point3 origin = point3(0.0, 0.0, 0.0);
+	vec3 horizontal = vec3(viewport_width, 0.0, 0.0);
+	vec3 vertical = vec3(0.0, viewport_height, 0.0);
+	point3 lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3(0.0, 0.0, focal_length);
+
+	// Render
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -13,7 +36,10 @@ int main()
 		std::cerr << "\rScanlines remainng: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++)
 		{
-			color pixel_color((double)(i) / (image_width - 1.0), (double)(j) / (image_height - 1.0), 0.25);
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
