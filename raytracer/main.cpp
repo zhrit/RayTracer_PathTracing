e5865683d18c05b8b72b0b73d3ab504bd4412d12
zhanghao11 <zhanghao11@corp.netease.com>
2021-06-11 17:20:57 +0800

    init

---
+++ raytracer/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "color.h"
+
+int main()
+{
+	const int image_width = 256;
+	const int image_height = 256;
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; j--)
+	{
+		std::cerr << "\rScanlines remainng: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++)
+		{
+			color pixel_color((double)(i) / (image_width - 1.0), (double)(j) / (image_height - 1.0), 0.25);
+			write_color(std::cout, pixel_color);
+		}
+	}
+	std::cerr << "\nDone.\n";
+}
